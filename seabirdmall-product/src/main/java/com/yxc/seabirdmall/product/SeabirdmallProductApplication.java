6d46882c6b59607d7d80d42502f311b026e6381a
Yuxuan Chen <chenyuxuang@gmail.com>
2022-08-08 18:07:16 -0700

    init: Initialize products micro-service module

---
+++ seabirdmall-product/src/main/java/com/yxc/seabirdmall/product/SeabirdmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.yxc.seabirdmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeabirdmallProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeabirdmallProductApplication.class, args);
+	}
+
+}
---
+++ seabirdmall-product/src/test/java/com/yxc/seabirdmall/product/SeabirdmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxc.seabirdmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeabirdmallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
